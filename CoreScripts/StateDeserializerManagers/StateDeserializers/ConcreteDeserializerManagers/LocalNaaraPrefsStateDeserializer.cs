672887852e55d400dd86265a99f9bb93b71ba8e2
Mateo Javier Bonnin <mateojavierbonnin@gmail.com>
2021-05-19 19:39:42 -0300

    - Fixed an issue with the prefs serializer not working as expected.

--- CoreScripts/StateDeserializerManagers/StateDeserializers/ConcreteDeserializerManagers/LocalNaaraPrefsStateDeserializer.cs
+++ CoreScripts/StateDeserializerManagers/StateDeserializers/ConcreteDeserializerManagers/LocalNaaraPrefsStateDeserializer.cs
@@ -1,14 +1,16 @@
 ﻿public class LocalNaaraPrefsStateDeserializer<T> : AbstractLocalStateDeserializer<T> where T : IStateSnapshot
 {
     private LocalJSONStateDeserializer<T> jsonDeserializer;
+    private string prefsKey;
 
     public LocalNaaraPrefsStateDeserializer(string prefsKey)
     {
-        this.jsonDeserializer = new LocalJSONStateDeserializer<T>(NaaraPlayerPrefs.GetString(prefsKey));
+        this.prefsKey = prefsKey;
     }
 
     public override T DeserializeState()
     {
+        this.jsonDeserializer = new LocalJSONStateDeserializer<T>(NaaraPlayerPrefs.GetString(prefsKey));
         return this.jsonDeserializer.DeserializeState();
     }
 }
