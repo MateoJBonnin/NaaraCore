e26102ec569ba6094ccffc9e06f59feddf2782d7
Mateo Javier Bonnin <mateojavierbonnin@gmail.com>
2021-11-24 10:29:16 -0300

    - Implemented new methods to the Utils extension methods.

--- CoreScripts/GameUtils/NaaraGameUtils.cs
+++ CoreScripts/GameUtils/NaaraGameUtils.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class NaaraGameUtils
+public static class NaaraGameUtils
 {
     public static Vector3 GetDirection(Vector3 originPoint, Vector3 destinationPoint)
     {
@@ -9,11 +9,16 @@ public class NaaraGameUtils
 
     public static float GetPercentageValue(float value, float percent)
     {
-        return (value / 100) * percent;
+        return value / 100 * percent;
     }
 
     public static bool IsCloseTo(Vector3 originPoint, Vector3 targetPoint, float distance)
     {
         return Vector3.Distance(originPoint, targetPoint) <= distance;
+    }
+
+    public static bool IsCloseTo(this Transform originPoint, Vector3 targetPoint, float distance)
+    {
+        return Vector3.Distance(originPoint.position, targetPoint) <= distance;
     }
 }
